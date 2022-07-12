b38e0f67b1c9ea5e958a6331877f06c302889dab
Muhammad Ibnu Prayogi <89898918+IbnuPrayogi@users.noreply.github.com>
2022-07-12 15:54:32 +0700

    role seeding

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,52 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama',
+        'email',
+        'password',
+        'nim',
+        'kelompok',
+        'roles_id',
+        'prodis_id',
+        'instagram'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+
+    public function roles(){
+        return $this->belongsTo(Role::class, 'roles_id');
+    }
+
+    public function prodis(){
+        return $this->belongsTo(Prodi::class, 'prodis_id');
+    }
 }
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     */
+    public function run()
+    {
+        $roles = [
+            [
+                'id' => 1,
+                'role' => 'Super Admin'
+            ],
+            [
+                'id' => 2,
+                'role' => 'Tugas'
+            ],
+            [
+                'id' => 3,
+                'role' => 'Daplok/Mentor'
+            ],
+            [
+                'id' => 4,
+                'role' => 'Pendanaan'
+            ],
+            [
+                'id' => 5,
+                'role' => 'Maba'
+            ],
+            [
+                'id' => 6,
+                'role' => 'Ormawa'
+            ]
+        ];
+        Role::query()->insert($roles);
+        
+    }
+};
