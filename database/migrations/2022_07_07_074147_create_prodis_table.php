18934483f2ba8d1f5597e9e3428f4400b82d158c
Aldi Nainggolan <88421969+RivaldiYN@users.noreply.github.com>
2022-07-12 13:47:31 +0700

    menambah role seeder & user seeder

--- database/migrations/2022_07_07_074147_create_prodis_table.php
+++ database/migrations/2022_07_07_074147_create_prodis_table.php
@@ -15,17 +15,17 @@ class CreateProdisTable extends Migration
     {
         Schema::create('prodis', function (Blueprint $table) {
             $table->id();
+            $table->string('namaProdi')->nullable();
             $table->string('kepalaProdi')->nullable();
             $table->string('akreditasi')->nullable();
             $table->integer('tahunBerdiri')->nullable();
             $table->string('ruangProdi')->nullable();
-            $table->integer('jumlahMahaSiswa')->nullable();
+            $table->integer('jumlahMahasiswa')->nullable();
             $table->string('diagramAlir')->nullable();
             $table->foreignId('ormawas_id')->nullable();
             $table->foreignId('jurusans_id')->nullable();
             $table->foreign('ormawas_id')->references('id')->on('ormawas')->onDelete('cascade');
             $table->foreign('jurusans_id')->references('id')->on('jurusans')->onDelete('cascade');
-
         });
     }
 
