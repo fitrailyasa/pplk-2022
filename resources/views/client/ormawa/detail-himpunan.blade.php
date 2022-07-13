865e9afa254bb896c3355757cde54f6dc4d76ad0
KhalilFaza <57977297+khalilfazza@users.noreply.github.com>
2022-07-13 11:24:03 +0700

    template client

---
+++ resources/views/client/ormawa/detail-himpunan.blade.php
@@ -0,0 +1,116 @@
+@extends('layouts.client.app')
+
+<!-- TITLE -->
+@section('title', 'Detail Himpunan')
+
+@section('style')
+    <!-- STYLES -->
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/himp-ukm.css">
+@endsection
+
+@section('content')
+    <!-- CONTENT -->
+    <div class="himpunan">
+        <img src="{{ asset('assets') }}/logo-contoh.png " class="hmp-img" alt="...">
+        <h2>HMP MANDALANATA</h2>
+        <h5>DIES NATALIS : 13 APRIL</h5>
+        <h5>PEMBINA HIMPUNAN : YUDHA RAHMAN</h5>
+        <h5>KETUA HIMPUNAN : ILHAM FAHREZI</h5>
+        <h2>VISI</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium dolore, rem vitae iure dolorem consequatur at? Cupiditate temporibus unde quaerat, adipisci minima illo error esse at commodi? Neque, amet porro aut fuga iste odio suscipit hic molestias ratione, excepturi cupiditate dolores consectetur ex veritatis in eum, laudantium debitis eaque facilis!</p>
+        <h2>MISI</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, quia commodi voluptate nobis ipsam molestiae quasi fugiat praesentium est, quis illum consectetur voluptatum? Quisquam, totam ullam quod dignissimos praesentium quidem. Veritatis nisi, alias quod corrupti quasi commodi sequi, provident optio voluptates error iure. Culpa, eveniet quo? Error ea natus neque!</p>
+        <h2> FILOSOFI LOGO</h2>
+        <h2>HMP MANDALANATA</h2>
+        <img src="{{ asset('assets') }}/logo-contoh.png " class="hmp-img2" alt="...">
+    </div>
+</div>
+<br><br>
+
+ <div class="slide-container swiper">
+            <div class="slide-content">
+                <div class="card-wrapper swiper-wrapper">
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+                        </div>
+                    </div>
+                    <div class="card-rr swiper-slide">
+
+                        <div class="card-content">
+                            <h2 class="name">David Dell</h2>
+                            <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="swiper-button-next swiper-navBtn"></div>
+            <div class="swiper-button-prev swiper-navBtn"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+@endsection
+
+@section('script')
+    <!-- SCRIPTS -->
+    <script></script>
+@endsection
