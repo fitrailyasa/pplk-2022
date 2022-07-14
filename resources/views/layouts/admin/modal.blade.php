9b1712d676e5a561a7ff2a57d69eaf0eb8848592
KhalilFaza <57977297+khalilfazza@users.noreply.github.com>
2022-07-14 11:53:03 +0700

    clearing

--- resources/views/layouts/admin/modal.blade.php
+++
@@ -1,8 +0,0 @@
-<script>
-    $("#myModal").on('hidden.bs.modal', function (e) {
-        $("#myModal .modal-dialog");
-    });
-    $(document).ready(function(){
-        $("#modal-sponsor").modal('show');
-    });    
-</script>
