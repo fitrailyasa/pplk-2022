92615c3d97d1a9f274b9d9756cb369ec77884916
Fitra Ilyasa <fitrailyasa12@gmail.com>
2022-07-14 16:12:24 +0700

    login regist

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,9 @@ Route::get('/game-home', function ()   {  // ===> home games
 Route::get('/login', function ()   {  // ===> Login
     return view('login');
 });
+Route::get('/registrasi', function ()   {  // ===> Registrasi
+    return view('registrasi');
+});
 
 
 
