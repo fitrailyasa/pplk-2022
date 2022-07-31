c278f20e8537d3ade7147114ff51bc2573873bf0
Aldi Nainggolan <88421969+RivaldiYN@users.noreply.github.com>
2022-08-01 00:09:56 +0700

    fix user cms

--- resources/views/admin/prodi/create.blade.php
+++ resources/views/admin/prodi/create.blade.php
@@ -46,7 +46,7 @@
           <div class="mb-3 row">
             <label class="col-sm-3 col-form-label">Tahun Berdiri</label>
             <div class="col-sm-9">
-              <input type="number" class="form-control" name="tahunBerdiri" id="tahunBerdiri"  >
+              <input type="number" class="form-control" placeholder="Tahun Berdiri" name="tahunBerdiri" id="tahunBerdiri"  >
             </div>
           </div>
           <div class="mb-3 row">
