eb44713843702a87e4ce255e547f120f58d70fb2
Fitra Ilyasa <fitrailyasa12@gmail.com>
2022-08-05 03:40:47 +0700

    update dikit

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,42 @@
 <?php
 
-use App\Http\Middleware\Ukm;
-use App\Http\Middleware\Maba;
-use App\Http\Middleware\Admin;
-use App\Http\Middleware\Staff;
-use App\Http\Middleware\DapMen;
-use App\Http\Middleware\Himpunan;
 use Illuminate\Routing\Controller;
-use App\Http\Middleware\SuperAdmin;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Middleware\Kedisiplinan;
-
-use App\Http\Middleware\DapMen;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Middleware\SuperAdmin;
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Himpunan;
+use App\Http\Middleware\Ukm;
+use App\Http\Middleware\Kedisiplinan;
+use App\Http\Middleware\DapMen;
+
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\AdminBegalinController;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AdminFunfactController;
+use App\Http\Controllers\Admin\AdminHimpunanController;
+use App\Http\Controllers\Admin\AdminKamusGaulController;
 use App\Http\Controllers\Admin\AdminUkmController;
 use App\Http\Controllers\Admin\AdminUptController;
 use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\Admin\AdminProdiController;
-
 use App\Http\Controllers\Admin\DapmenUserController;
-use App\Http\Controllers\Client\ClientUkmController;
-use App\Http\Controllers\Client\ClientUptController;
-use App\Http\Controllers\Admin\AdminBegalinController;
-use App\Http\Controllers\Admin\AdminFunfactController;
-use App\Http\Controllers\Client\ClientProdiController;
-use App\Http\Controllers\Admin\AdminHimpunanController;
-use App\Http\Controllers\Admin\AdminKamusGaulController;
+
 use App\Http\Controllers\Client\ClientBegalinController;
 use App\Http\Controllers\Client\ClientBiodataController;
 use App\Http\Controllers\Client\ClientJurusanController;
 use App\Http\Controllers\Client\ClientKabinetController;
+use App\Http\Controllers\Client\ClientKamusgaulController;
+use App\Http\Controllers\Client\ClientKodeGameController;
 use App\Http\Controllers\Client\ClientKeluhanController;
-use App\Http\Controllers\Client\ClientScannerController;
 
 use App\Http\Controllers\Client\ClientHimpunanController;
-
 use App\Http\Controllers\Client\ClientUkmController;
 
 use App\Http\Controllers\Client\ClientProdiController;
 use App\Http\Controllers\Client\ClientScannerController;
 use App\Http\Controllers\Client\ClientUptController;
 use App\Http\Controllers\Leaderboardcontroller;
-
-use App\Http\Controllers\Client\ClientKodeGameController;
-use App\Http\Controllers\Client\ClientKamusgaulController;
-use App\Http\Controllers\Client\ClientGameTebakBangunanController;
-
 
 
 
@@ -161,109 +149,55 @@ Route::middleware([DapMen::class])->group(function () {
 Route::middleware(['auth'])->group(function () {
 
 
-  // UTAMA
-  Route::get('/', [ClientBegalinController::class, 'index']);
-  Route::get('/beranda', [ClientBegalinController::class, 'index']);
-  Route::get('/upt', [ClientUptController::class, 'index'])->name('upt');
-  Route::get('/kabinet', [ClientKabinetController::class, 'index'])->name('kabinet');
-  Route::get('/senat', function () {
-    return view('client.senat');
-  });
-  Route::get('/kamus-gaul', [ClientKamusgaulController::class, 'index'])->name('kamus-gaul');
+    // UTAMA
+    Route::get('/', [ClientBegalinController::class, 'index']);
+    Route::get('/beranda', [ClientBegalinController::class, 'index']);
+    Route::get('/upt', [ClientUptController::class, 'index'])->name('upt');
+    Route::get('/kabinet', [ClientKabinetController::class, 'index'])->name('kabinet');
+    Route::get('/senat', function () { return view('client.senat'); });
+    Route::get('/kamus-gaul', [ClientKamusgaulController::class, 'index'])->name('kamus-gaul');
 
-  // BOOKLET
-  Route::get('/booklet', function () {
-    return view('client.booklet');
-  });
+    // BOOKLET
+    Route::get('/booklet', function () { return view('client.booklet'); });
 
-  // PPLK
-  Route::get('/pplk', function () {
-    return view('client.pplk');
-  });
-  Route::get('/div-pplk', function () {
-    return view('client.div-pplk');
-  });
+    // PPLK
+    Route::get('/pplk', function () { return view('client.pplk'); });
+    Route::get('/div-pplk', function () { return view('client.div-pplk'); });
 
-  // BIODATA
-  Route::get('/biodata', [ClientBiodataController::class, 'index'])->name('biodata');
-  Route::get('/edit-biodata', [ClientBiodataController::class, 'indexEditBio'])->name('edit-biodata');
-  Route::get('edit-fotoProfil/{id}', [ClientBiodataController::class, 'editProfil']);
-  Route::put('update-fotoProfil/{id}', [ClientBiodataController::class, 'updateProfil']);
-  Route::get('edit-profil/{id}', [ClientBiodataController::class, 'editBiodata']);
-  Route::put('update-profil/{id}', [ClientBiodataController::class, 'updateBiodata']);
+    // BIODATA
+    Route::get('/biodata', [ClientBiodataController::class, 'index'])->name('biodata');
+    Route::get('/edit-biodata', [ClientBiodataController::class, 'indexEditBio'])->name('edit-biodata');
+    Route::get('edit-fotoProfil/{id}', [ClientBiodataController::class, 'editProfil']);
+    Route::put('update-fotoProfil/{id}', [ClientBiodataController::class, 'updateProfil']);
+    Route::get('edit-profil/{id}', [ClientBiodataController::class, 'editBiodata']);
+    Route::put('update-profil/{id}', [ClientBiodataController::class, 'updateBiodata']);
 
-  // FORM KELUHAN
-  Route::get('/form-keluhan ', [ClientKeluhanController::class, 'index'])->name('indexKeluhan');
-  Route::post('/form-keluhan/{id}', [ClientKeluhanController::class, 'create'])->name('create-keluhan');
+    // FORM KELUHAN
+    Route::get('/form-keluhan ', [ClientKeluhanController::class, 'index'])->name('indexKeluhan');
+    Route::post('/form-keluhan/{id}', [ClientKeluhanController::class, 'create'])->name('create-keluhan');
 
-  // ORMAWA
-  Route::get('/ukm', function () {
-    return view('client.ormawa.ukm');
-  });
-  Route::get('/himpunan', function () {
-    return view('client.ormawa.himpunan');
-  });
-  Route::get('/detail-himpunan', function () {
-    return view('client.ormawa.detail-himpunan');
-  });
-  Route::get('/jurusan', [ClientJurusanController::class, 'index'])->name('jurusan');
-  Route::get('/prodi', function () {
-    return view('client.jurusan.prodi');
-  });
-  Route::get('/detail-prodi',)->name('detailprodi');
+    // ORMAWA
+    Route::get('/ukm', function () { return view('client.ormawa.ukm'); });
+    Route::get('/himpunan', function () { return view('client.ormawa.himpunan'); });
+    Route::get('/detail-himpunan', function () { return view('client.ormawa.detail-himpunan'); });
+    Route::get('/jurusan', [ClientJurusanController::class, 'index'])->name('jurusan');
+    Route::get('/prodi', function () { return view('client.jurusan.prodi'); });
+    Route::get('/detail-prodi',)->name('detailprodi');
 
-  // GAMES
-  Route::get('/game-home', [LeaderboardController::class, 'index']);
-  Route::get('/card-list', [ClientKodeGameController::class, 'index']);
-  Route::get('/redeem/{no}', [ClientKodeGameController::class, 'show']);
-  Route::get('/redeem-failed', function () {
-    return view('client.games.redeem-code.failed');
-  });
-  Route::get('/redeem-success', function () {
-    return view('client.games.redeem-code.success');
-  });
+    // GAMES
+    Route::get('/game-home', [LeaderboardController::class, 'index']);
+    Route::get('/card-list', [ClientKodeGameController::class, 'index']);
+    Route::get('/redeem/{no}', [ClientKodeGameController::class, 'show']);
+    Route::get('/redeem-failed', function () { return view('client.games.redeem-code.failed'); });
+    Route::get('/redeem-success', function () { return view('client.games.redeem-code.success'); });
+    Route::post('/submitcode/{id}', [ClientKodeGameController::class, 'sumscore'])->name('sumscore');
+    Route::get('/tebak-bangunan', function () { return view('client.games.tebak-bangunan.home'); });
+    Route::get('/tebak-bangunan-game', function () { return view('client.games.tebak-bangunan.game'); });
 
-  Route::post('/submitcode/{id}', [ClientKodeGameController::class, 'sumscore'])->name('sumscore');
-  Route::get('/tebak-bangunan', function () {
-    return view('client.games.tebak-bangunan.home');
-  });
-  Route::get('/tebak-bangunan-game', function () {
-    return view('client.games.tebak-bangunan.game');
-  });
+    // OUR TEAM
+    Route::get('/our-team', function () { return view('client.our-team'); });
 
-  // OUR TEAM
-  Route::get('/our-team', function () {
-    return view('client.our-team');
-
-
-
-//TEBAK BANGUNAN
-  Route::get('/tebak-bangunan',[ClientGameTebakBangunanController::class,'index']);     // ===> tebak bangunan home
-  Route::get('/tebak-bangunan-game/{id}',[ClientGameTebakBangunanController::class,'show']);
-  Route::get('/tebak-bangunan-game/{id}/{jawaban}',[ClientGameTebakBangunanController::class,'store']);
-  Route::get('/tebak-bangunan-selesai/{id}',[ClientGameTebakBangunanController::class,'restart']);
-
-
-
-
-
-
-  Route::get('/game-home', function () {            // ===> home games
-    return view('client.games.games');
-  });
-  Route::get('/senat', function () {  // ===> Registrasi
-    return view('client.senat');
-
-  });
-
-
-Route::get('/prodi', [ClientProdiController::class, 'index'])->name('prodis'); // ===> prodi
-Route::get('/himpunan', [ClientHimpunanController::class, 'index'])->name('himpunans'); // ===> himpunan
-Route::get('/ukm', [ClientUkmController::class, 'index'])->name('ukms'); // ===> ukm
-
-Route::get('/detail-prodi/{id}', [ClientProdiController::class, 'show'])->name('detailProdi');
-Route::get('/detailHimpunan/{id}', [ClientHimpunanController::class, 'show'])->name('detailHimpunan');
-Route::get('/detailUkm/{id}', [ClientUkmController::class, 'show'])->name('detailUkm');
+});
 
 
 // Route::get('/gen', [ClientBiodataController::class, 'generateAllQrCode']);
