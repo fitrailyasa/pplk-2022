119fccfa076ab8f7eb696d83e473d94eeb7680c4
Fitra Ilyasa <fitrailyasa12@gmail.com>
2022-08-08 04:44:42 +0700

    edit lagi scanner role

--- resources/views/scanner/ScannerPolling.blade.php
+++ resources/views/scanner/ScannerPolling.blade.php
@@ -20,7 +20,12 @@
                 <div class="col-top-scanner justify-content-center">
                     <h5> SCANNER POLLING </h5>
                 </div>
-                <div><form action="/polling/{{ $ukms->id }}" method="POST">
+                <div>
+                @if(auth()->user()->roles_id == 1)
+                    <form action="super/polling/{{ $ukms->id }}" method="POST">
+                @elseif(auth()->user()->roles_id == 4)
+                    <form action="ukms/polling/{{ $ukms->id }}" method="POST">
+                @endif
                     @csrf
                 <div class="col-scanner justify-content-center" id="ganti">
                     <div id="reader"  class="justify-content-center"></div>
@@ -36,8 +41,8 @@
 @section('script')
     <!-- SCRIPTS -->
     <!-- QrCode Js -->
-<script src="{{ asset('assets/js/html5-qrcode.min.js') }}" type="text/javascript"></script>
-<script  src="{{ asset('assets/js/scanner-polling-main.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/js/html5-qrcode.min.js') }}" type="text/javascript"></script>
+    <script  src="{{ asset('assets/js/scanner-polling-main.js') }}" type="text/javascript"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
