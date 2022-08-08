c17d59e14cc083a07797ed0abf981f1999784243
Fitra Ilyasa <fitrailyasa12@gmail.com>
2022-08-09 04:22:04 +0700

    edit lagi ya

--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -26,7 +26,6 @@
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
         @yield('style')
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
         <!-- Owl-carousel -->
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
