9fea5f84ea755bb9faf2c57472fea0d862f41fec
Fitra Ilyasa <fitrailyasa12@gmail.com>
2022-08-09 04:27:37 +0700

    edit lagi lagi

--- resources/views/layouts/client/app.blade.php
+++ resources/views/layouts/client/app.blade.php
@@ -25,6 +25,7 @@
         <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
         @yield('style')
 
         <!-- Owl-carousel -->
